80e3ba2aa3ff21e5d9dea7574c0c5b3250a09011
Ziaad-Khaled <77291238+Ziaad-Khaled@users.noreply.github.com>
2022-10-25 11:09:35 +0200

    date picker colors changed and singlesmsform made

--- src/app/components/bulk/bulk-campaign-submission/bulk-campaign-submission.component.ts
+++ src/app/components/bulk/bulk-campaign-submission/bulk-campaign-submission.component.ts
@@ -1,32 +1,65 @@
-import { Component, OnInit,NgZone, ViewChild } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Component, OnInit,NgZone, ViewChild, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatGridTileHeaderCssMatStyler } from '@angular/material/grid-list';
+import { Subscription } from 'rxjs';
 import { DialogueBoxComponent } from '../../dialogue-box/dialogue-box.component';
 @Component({
   selector: 'app-bulk-campaign-submission',
   templateUrl: './bulk-campaign-submission.component.html',
   styleUrls: ['./bulk-campaign-submission.component.scss']
 })
-export class BulkCampaignSubmissionComponent implements OnInit {
+export class BulkCampaignSubmissionComponent implements OnInit, OnDestroy {
+
+
+
+  singleSMSForm! : FormGroup;
+  private subscriptions = new Subscription();
 
   smsBody!: string;
-
   minDate: Date;
   currentDate:any = new Date();
-  
-  constructor(public dialog: MatDialog) {
+
+  constructor(public dialog: MatDialog, private fb: FormBuilder) {
     const currentYear = new Date().getFullYear();
     this.minDate = new Date(currentYear - 0, 0, 1);
    }
+  
 
   ngOnInit(): void {
+    this.singleSMSForm = this.fb.group({
+      senderName: new FormControl<string>('', Validators.required),
+      date: new FormControl('', Validators.required),
+      time: new FormControl('', Validators.required),
+      smsBody: new FormControl('', Validators.required),
+      testDial: new FormControl('')
+    });
+
+    this.subscriptions.add(this.singleSMSForm.valueChanges.subscribe(val => {
+      console.log("single SMS form: ")
+      console.log(val);
+      
+      //on valid send the sms to the dials 
+    }));
+
+    this.subscriptions.add(this.singleSMSForm.get("smsBody")?.valueChanges.subscribe(val => {
+      this.smsBody = this.singleSMSForm.get("smsBody")?.value;
+    }));
   }
+
+
   openDialog(): void {
     this.dialog.open(DialogueBoxComponent,{
       width:'350px',
       data:"right click"
     })
     
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
   
 }
